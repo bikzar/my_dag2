37079224868a4c6210ff5c173f7572fbf2141caa
Siarhei Vaitsiankou <Siarhei_Vaitsiankou@epam.com>
2020-04-03 11:31:58 +0300

    Remove redundant args.

--- airflow/print_data_DAG.py
+++ airflow/print_data_DAG.py
@@ -1,20 +1,16 @@
-from datetime import timedelta, datetime
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'bikzar',
-    'depends_on_past': False,
-    'start_date': days_ago(2),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'start_date': days_ago(1),
 }
 
 dag = DAG(
     'my_simple_dag',
     default_args=default_args,
-    description='A simple DAG',
     schedule_interval='* */20 * * *',
 )
 
